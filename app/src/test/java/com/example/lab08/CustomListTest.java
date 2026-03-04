29297d8f6af3d7c2f3928ff603976d52f551d8a1
Brandon Ledohowski <b.ledohowski@share.epsb.ca>
2026-03-03 17:46:16 -0700

    failing deleteCity test commit

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,13 @@ public class CustomListTest {
         // This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+        list.addCity(edmonton);
+        list.deleteCity(edmonton);
+        assertFalse(list.hasCity(edmonton));
+    }
 }
