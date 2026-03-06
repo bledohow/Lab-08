25230f59edaa6221e59ee0168ad490bf405e36ce
Brandon Ledohowski <b.ledohowski@share.epsb.ca>
2026-03-06 11:13:56 -0700

    successful deleteCity test commit

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -1,6 +1,7 @@
 package com.example.lab08;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class CustomList {
@@ -14,9 +15,18 @@ public class CustomList {
         cities.add(city);
     }
 
+    public void deleteCity(City city) { //adapted from https://www.baeldung.com/java-concurrentmodificationexception
+        for (Iterator<City> iterator = cities.iterator(); iterator.hasNext();) {
+            City cityInList = iterator.next();
+            if (cityInList.getCityName().equals(city.getCityName())) {
+                if (cityInList.getProvinceName().equals(city.getProvinceName())) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
+
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
-
-    // Will be implemented later using TDD (leave blank for now)
 }
